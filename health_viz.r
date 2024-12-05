186e11d8e61719af638e3a949586c436e5dc1376
arronlacey <a.s.lacey@swansea.ac.uk>
2024-12-05 15:26:23 +0000

    changed bmi hist theme and labels

--- health_viz.r
+++ health_viz.r
@@ -20,7 +20,9 @@ health_data <- NHANES %>% select(BMI,Age,Gender,TotChol)
 # Create a historgram of BMI using ggplot
 
 ggplot(health_data, aes(x = BMI)) +
-  geom_histogram(binwidth = 1)
+  geom_histogram(binwidth = 1, fill = "skyblue", colour = "black") +
+  labs(title = "Mean height in Wales is higher than European Average") +
+  theme_minimal()
 
 
 
